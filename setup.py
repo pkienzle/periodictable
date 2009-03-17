7cf0f344e76ed1bc6d18479b1bea9393e64755e0
pkienzle <pkienzle@ab603e80-ab4e-4d97-b1f8-5903cef66d6b>
2009-03-17 16:14:33 +0000

    elements: refactor molecule->forumla, periodic_table->elements, elements package->periodictable package (step 3)

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup, find_packages
 import fix_setuptools_chmod
 
 dist = setup(
-        name = 'elements',
-        version = '1.1',
+        name = 'periodictable',
+        version = '0.9',
         packages = find_packages(),
-        package_data = {'elements' : ['xsf/*']},
+        package_data = {'periodictable' : ['xsf/*']},
         install_requires = ['pyparsing'],
 )
 

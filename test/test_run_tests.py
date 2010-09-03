61d133fceec3e7c9cf710668d1e5b3e821dfb201
npatel <npatel@ab603e80-ab4e-4d97-b1f8-5903cef66d6b>
2010-09-03 16:30:34 +0000

    elements: remove old test driver

--- test/test_run_tests.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-import os
-for f in ['core','covalent_radius','crystal_structure',
-          'density','formulas','magnetic_ff','mass',
-          'nsf','private','xsf']:
-    os.system(sys.executable+" "+f+".py")

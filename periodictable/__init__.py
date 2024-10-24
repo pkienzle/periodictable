ca94dbd6f132be7c660e9d905e2db600e8dc1c2f
Paul Kienzle <pkienzle@nist.gov>
2024-10-24 11:35:26 -0400

    Tweak lazy loading implementation. Refs #72.

--- periodictable/__init__.py
+++ periodictable/__init__.py
@@ -28,11 +28,55 @@ whose conditions may differ from those of your experiment.
 __docformat__ = 'restructuredtext en'
 __version__ = "2.0.0-pre"
 
+__all__ = ['elements'] # Lazy symbols and individual elements added later
+
 import importlib
 
 from . import core
 from . import mass
 from . import density
+
+_LAZY_MODULES = [
+    'activation',
+    'covalent_radius',
+    'crystal_structure',
+    'nsf',
+    'xsf',
+    'magnetic_ff',
+    ]
+_LAZY_LOAD = {
+    'formula': 'formulas',
+    'mix_by_weight': 'formulas',
+    'mix_by_volume': 'formulas',
+    'neutron_sld': 'nsf',
+    'neutron_scattering': 'nsf',
+    'xray_sld': 'xsf',
+}
+def __getattr__(name: str):
+    """
+    Lazy loading of modules and symbols from other modules. This is
+    equivalent to using "from .formulas import formula" etc in __init__
+    except that the import doesn't happen until the symbol is referenced.
+    Using "from periodictable import formula" will import the symbol immediately.
+    "from periodictable import *" will import all symbols, including the lazy
+    """
+    module_name = _LAZY_LOAD.get(name, None)
+    if module_name is not None:
+        # Lazy symbol: fetch name from the target module
+        #print(f"from {__name__}.{module_name} import {name} [lazy]")
+        module = importlib.import_module(f'{__name__}.{module_name}')
+        symbol = getattr(module, name)
+        globals()[name] = symbol
+        return symbol
+    if name in _LAZY_MODULES:
+        # Lazy module: just need to import it
+        #print(f"import {__name__}.{name} [lazy]")
+        return importlib.import_module(f'{__name__}.{name}')
+    raise AttributeError(f"module '{__name__}' has not attribute '{name}'")
+def __dir__():
+    return __all__
+# Support 'from periodictable import *' and 'dir(periodictable)'
+__all__ = [*__all__, *_LAZY_MODULES, *_LAZY_LOAD.keys()]
 
 # Always make mass and density available
 elements = core.PUBLIC_TABLE
@@ -40,7 +84,10 @@ mass.init(elements)
 density.init(elements)
 del mass, density
 
-# Lazy loading of other element and isotope attributes
+# Add element name and symbol (e.g. nickel and Ni) to the public attributes.
+__all__ += core.define_elements(elements, globals())
+
+# Lazy loading of element and isotope attributes, e.g., Ni.covalent_radius
 core.delayed_load(
     ['covalent_radius', 'covalent_radius_units', 'covalent_radius_uncertainty'],
     lambda: covalent_radius.init(elements))
@@ -64,50 +111,6 @@ core.delayed_load(
 core.delayed_load(
     ['magnetic_ff'],
     lambda: magnetic_ff.init(elements))
-
-# Lazy loading of modules and symbols from other modules. This is
-# equivalent to using "from .formulas import formula" etc in __init__
-# except that the import doesn't happen until the symbol is referenced.
-# Using "from periodictable import formula" will import the symbol immediately.
-# "from periodictable import *" will import all symbols, including the lazy
-_LAZY_MODULES = [
-    'activation',
-    'covalent_radius',
-    'crystal_structure',
-    'nsf',
-    'xsf',
-    'magnetic_ff',
-    ]
-_LAZY_LOAD = {
-    'formula': 'formulas',
-    'mix_by_weight': 'formulas',
-    'mix_by_volume': 'formulas',
-    'neutron_sld': 'nsf',
-    'neutron_scattering': 'nsf',
-    'xray_sld': 'xsf',
-}
-def __getattr__(name: str):
-    module_name = _LAZY_LOAD.get(name, None)
-    if module_name is not None:
-        # Attr is in the lazy list: fetch name from the target module
-        module = importlib.import_module(f'{__name__}.{module_name}')
-        symbol = getattr(module, name)
-        globals()[name] = symbol
-        return symbol
-    if name in _LAZY_MODULES:
-        return importlib.import_module(f'{__name__}.{name}')
-    raise AttributeError(f"{__name__}.{name} not found")
-def __dir__():
-    return __all__
-
-# Export variables for each element name and symbol.
-__all__ = core.define_elements(elements, globals())
-__all__ = [
-    'elements',
-    *__all__,
-    *sorted(_LAZY_MODULES),
-    *sorted(_LAZY_LOAD.keys()),
-    ]
 
 # Data needed for setup.py when bundling the package into an exe
 def data_files():
